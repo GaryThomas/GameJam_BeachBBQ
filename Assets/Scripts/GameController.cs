4b3a864a4bbdd0e8f037d5e079753f3dcdfa65e3
Gary Thomas <gary@mlbassoc.com>
2020-04-12 10:40:05 +0200

    Update for 2018.4.2f1

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : Singleton<GameController> {
+    private bool _ready = false;
+    private bool _playing = false;
+
+    private void Start() {
+        StartCoroutine(Game());
+    }
+
+    IEnumerator Game() {
+        yield return StartCoroutine(BeginLevel());
+        yield return StartCoroutine(PlayLevel());
+        yield return StartCoroutine(EndLevel());
+    }
+
+    IEnumerator BeginLevel() {
+        yield return new WaitForSeconds(2f);
+    }
+
+    IEnumerator PlayLevel() {
+        yield return new WaitForSeconds(2f);
+    }
+
+    IEnumerator EndLevel() {
+        yield return new WaitForSeconds(2f);
+    }
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+    private static T m_instance;
+    public static T Instance {
+        get {
+            if (m_instance == null) {
+                m_instance = GameObject.FindObjectOfType<T>();
+                if (m_instance == null) {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    singleton.AddComponent<T>();
+                }
+            }
+            return m_instance;
+        }
+    }
+
+    public virtual void Awake() {
+        Debug.Log("Singleton: " + typeof(T).Name);
+        if (m_instance == null) {
+            m_instance = this as T;
+            // DontDestroyOnLoad(gameObject);
+        } else if (m_instance != this) {
+            Debug.Log("Singleton: destroy extra object");
+            Destroy(gameObject);
+        }
+    }
+}
