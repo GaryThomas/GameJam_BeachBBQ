4909360656d97a7d1ec0b684369c4af91c9db018
Gary Thomas <gary@mlbassoc.com>
2020-04-12 12:48:06 +0200

    Improve basic game controller scaffold

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,30 +1,78 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState {
+    GameInit,
+    GameInitComplete,
+    GameOver,
+    BeginLevelComplete,
+    PlayLevelComplete,
+    EndLevelComplete,
+}
+
 public class GameController : Singleton<GameController> {
-    private bool _ready = false;
-    private bool _playing = false;
+    public event Action OnInitGame;
+    public event Action OnBeginLevel;
+    public event Action OnPlayLevel;
+    public event Action OnEndLevel;
+
+    private GameState _state;
 
     private void Start() {
         StartCoroutine(Game());
     }
 
     IEnumerator Game() {
-        yield return StartCoroutine(BeginLevel());
-        yield return StartCoroutine(PlayLevel());
-        yield return StartCoroutine(EndLevel());
+        yield return StartCoroutine(InitGame());
+        while (_state != GameState.GameOver) {
+            yield return StartCoroutine(BeginLevel());
+            yield return StartCoroutine(PlayLevel());
+            yield return StartCoroutine(EndLevel());
+        }
+    }
+
+    IEnumerator InitGame() {
+        _state = GameState.GameInit;
+        OnInitGame?.Invoke();
+        yield return null;
     }
 
     IEnumerator BeginLevel() {
-        yield return new WaitForSeconds(2f);
+        if (OnBeginLevel != null) {
+            OnBeginLevel.Invoke();
+        } else {
+            _state = GameState.BeginLevelComplete;
+        }
+        while (_state != GameState.BeginLevelComplete) {
+            yield return null;
+        }
     }
 
     IEnumerator PlayLevel() {
-        yield return new WaitForSeconds(2f);
+        if (OnPlayLevel != null) {
+            OnPlayLevel.Invoke();
+        } else {
+            _state = GameState.PlayLevelComplete;
+        }
+        while (_state != GameState.PlayLevelComplete) {
+            yield return null;
+        }
     }
 
     IEnumerator EndLevel() {
-        yield return new WaitForSeconds(2f);
+        if (OnEndLevel != null) {
+            OnEndLevel.Invoke();
+        } else {
+            _state = GameState.EndLevelComplete;
+        }
+        while (_state != GameState.EndLevelComplete) {
+            yield return null;
+        }
+    }
+
+    public void UpdateGameState(GameState newState) {
+        _state = newState;
     }
 }
