aa8f85007af038dd640bb91be44b00152b4cdd5d
Gary Thomas <gary@mlbassoc.com>
2020-04-25 12:48:21 +0200

    Building up level - police cars non-functional so far

--- Assets/Scripts/CrowdMember.cs
+++ Assets/Scripts/CrowdMember.cs
@@ -6,7 +6,7 @@ public class CrowdMember : MonoBehaviour {
 
     [SerializeField] float followSpeed = 2f;
     [SerializeField] float minDistance = 2f;  // Separation from player
-    [SerializeField] float zombieTime = 5f; // Time keep following
+    [SerializeField] float zombieTime = 15f; // Time keep following
 
     private bool _following;
     private GameObject _player;
@@ -24,6 +24,7 @@ public class CrowdMember : MonoBehaviour {
         if (_zombieTimer <= 0) {
             _following = false;
             _sr.color = new Color(_sr.color.r, _sr.color.g, _sr.color.b, 1.0f);
+            _rb2d.velocity = Vector3.zero;
         }
         if (_following) {
             float dist = Vector3.Distance(transform.position, _player.transform.position);
