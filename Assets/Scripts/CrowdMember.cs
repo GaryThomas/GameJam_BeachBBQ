3249776239bc617b04c46fe633f1ece5be7809b5
Gary Thomas <gary@mlbassoc.com>
2020-04-25 12:00:04 +0200

    Importing some other cars.  Work on setting up characters

--- Assets/Scripts/CrowdMember.cs
+++ Assets/Scripts/CrowdMember.cs
@@ -4,4 +4,45 @@ using UnityEngine;
 
 public class CrowdMember : MonoBehaviour {
 
+    [SerializeField] float followSpeed = 2f;
+    [SerializeField] float minDistance = 2f;  // Separation from player
+    [SerializeField] float zombieTime = 5f; // Time keep following
+
+    private bool _following;
+    private GameObject _player;
+    private Rigidbody2D _rb2d;
+    private SpriteRenderer _sr;
+    private float _zombieTimer;
+
+    private void Awake() {
+        _rb2d = GetComponent<Rigidbody2D>();
+        _sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate() {
+        _zombieTimer -= Time.fixedDeltaTime;
+        if (_zombieTimer <= 0) {
+            _following = false;
+            _sr.color = new Color(_sr.color.r, _sr.color.g, _sr.color.b, 1.0f);
+        }
+        if (_following) {
+            float dist = Vector3.Distance(transform.position, _player.transform.position);
+            if (dist > minDistance) {
+                Vector3 newPos = Vector3.MoveTowards(transform.position, _player.transform.position, followSpeed * Time.fixedDeltaTime);
+                _rb2d.MovePosition(newPos);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            if (!_following) {
+                _following = true;
+                _player = other.gameObject;
+                _sr.color = new Color(_sr.color.r, _sr.color.g, _sr.color.b, 0.75f);
+                _zombieTimer = zombieTime;
+                Debug.Log("Touched by player!");
+            }
+        }
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
+    [SerializeField] float moveSpeed = 4f;
 
+    private Vector3 _movement;
+    private Rigidbody2D _rb2d;
+
+    private void Awake() {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        _movement = new Vector3(horiz, vert, 0);
+    }
+
+    private void FixedUpdate() {
+        _rb2d.MovePosition(transform.position + _movement * Time.fixedDeltaTime);
+    }
 }
