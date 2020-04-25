e1d3e6b3dd79d45cfe76f5a1b9230a232c4fc8aa
Gary Thomas <gary@mlbassoc.com>
2020-04-25 13:11:59 +0200

    Make sure crowd stops once beached of not following player

--- Assets/Scripts/CrowdMember.cs
+++ Assets/Scripts/CrowdMember.cs
@@ -9,6 +9,7 @@ public class CrowdMember : MonoBehaviour {
     [SerializeField] float zombieTime = 15f; // Time keep following
 
     private bool _following;
+    private bool _beached;
     private GameObject _player;
     private Rigidbody2D _rb2d;
     private SpriteRenderer _sr;
@@ -24,7 +25,6 @@ public class CrowdMember : MonoBehaviour {
         if (_zombieTimer <= 0) {
             _following = false;
             _sr.color = new Color(_sr.color.r, _sr.color.g, _sr.color.b, 1.0f);
-            _rb2d.velocity = Vector3.zero;
         }
         if (_following) {
             float dist = Vector3.Distance(transform.position, _player.transform.position);
@@ -32,18 +32,27 @@ public class CrowdMember : MonoBehaviour {
                 Vector3 newPos = Vector3.MoveTowards(transform.position, _player.transform.position, followSpeed * Time.fixedDeltaTime);
                 _rb2d.MovePosition(newPos);
             }
+        } else {
+            _rb2d.velocity = Vector3.zero;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
-            if (!_following) {
+            if (!_following && !_beached) {
                 _following = true;
                 _player = other.gameObject;
                 _sr.color = new Color(_sr.color.r, _sr.color.g, _sr.color.b, 0.75f);
                 _zombieTimer = zombieTime;
                 Debug.Log("Touched by player!");
             }
+        } else if (other.gameObject.tag == "Beach") {
+            _following = false;
+            _beached = true;
+            _rb2d.velocity = Vector3.zero;
+            _sr.color = new Color(_sr.color.r, _sr.color.g, _sr.color.b, 1.0f);
+            _zombieTimer = zombieTime;
+            Debug.Log("Made it to the beach!");
         }
     }
 }
